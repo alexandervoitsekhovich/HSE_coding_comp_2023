56ec6e1b8472d4b75046d8da7040d7d0f1220c8d
Alexander Voitsekhovich <95379952+alexandervoitsekhovich@users.noreply.github.com>
2023-04-03 03:20:32 +0400

    C++ solution for 1st task added!
    .exe file extention is also included.

---
+++ app.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int arr_size;
+    cin >> arr_size;
+    int* arr = new int[arr_size];
+
+    for (int i = 0; i < arr_size; i++) {
+        cin >> arr[i];
+    }
+
+    int counter = 0;
+
+    for (int i = 0; i < arr_size; i++) {
+        int number = arr[i];
+        if (number % 4 == 0) {
+            counter += number / 4;
+        } else {
+            counter += number / 4;
+            if (number % 4 == 2 || number % 4 == 3) {
+                counter += 2;
+            } else {
+                counter += 1;
+            }
+        }
+    }
+
+    cout << counter;
+}
